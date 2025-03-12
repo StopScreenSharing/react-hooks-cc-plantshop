26ec5fb9e6510e876fd8f4d3769acf0a97dc4481
aidadieudonne <aidaharing10@gmail.com>
2025-03-12 17:50:35 -0600

    made POST deliverable

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -12,9 +12,27 @@ function PlantPage() {
     .then((plants) => setPlants(plants))
   }, [])
 
+  const handleAddPlant = (newPlant) => {
+    fetch('http://127.0.0.1:6001/plants', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: newPlant.name,
+      image: newPlant.image,
+      price: newPlant.price
+    })
+  })
+  .then(response => response.json())
+  .then(addedPlants => {
+    setPlants([...plants, addedPlants]);
+  });
+  };
+
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
       <Search />
       <PlantList plants={plants}/>
     </main>
