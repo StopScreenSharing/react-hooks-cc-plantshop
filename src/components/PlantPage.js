1d9a1f6bcd945587aebb8af65a4d110a19385640
aidadieudonne <aidaharing10@gmail.com>
2025-03-12 17:24:23 -0600

    made a GET request to plants

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:6001/plants')
+    .then(r => r.json())
+    .then((plants) => setPlants(plants))
+  }, [])
   return (
     <main>
       <NewPlantForm />
